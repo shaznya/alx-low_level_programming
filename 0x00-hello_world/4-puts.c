38f40ecb601d1715ef3da18bc37f895761a2feda
shanya <shaznyagol@gmail.com>
2023-04-20 08:28:59 -0700

    print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
 /**
   *main-entry point
+  *
   *return:always 0(success)
   */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+	/*return:always 0(success)*/
 	return (0);
 }
