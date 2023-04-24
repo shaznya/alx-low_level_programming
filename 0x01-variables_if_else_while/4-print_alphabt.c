9f769d05646262c24c828f4cc7be216b4e131e20
shanya <shaznyagol@gmail.com>
2023-04-24 02:06:41 -0700

    base10

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+  *mai - Entry point
+  print alphabets in lowercase except q and e
+  *Return: Always 0(success)
+  */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122);
+	{
+		if (n == 101 || n ==113)
+		{
+			n++;
+			continue;
+		}
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
