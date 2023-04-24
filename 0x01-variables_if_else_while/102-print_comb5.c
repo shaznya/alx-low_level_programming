079bc8fff2cf9b7d97c87225ecde00ade3b0c370
shanya <shaznyagol@gmail.com>
2023-04-24 08:17:11 -0700

    print

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,4 +1,29 @@
 #include <stdio.h>
 /**
   *main - Entry point
-  *
+  *possible combinations of two two-digit numbers
+  *Return: Always 0
+  */
+int main(void)
+{
+	int p, q;
+
+	for (p = 0; p <= 98; p++)
+	{
+		for (q = p + 1; q <= 99; q++)
+		{
+			putchar((p / 10) + '0');
+			putchar((p % 10) + '0');
+			putchar(' ');
+			putchar((q / 10) + '0');
+			putchar((q % 10) + '0');
+			if (p == 98 && 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
+
