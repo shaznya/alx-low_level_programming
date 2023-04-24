0c62d797cd59602b88c71e22e5fc63b76c5cffda
shanya <shaznyagol@gmail.com>
2023-04-24 05:56:05 -0700

    last

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,12 +13,12 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	lastDigit = n % 10;
+	ld = n % 10;
 	if (ld > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, ld);
 	}
-	else if (lastDigit == 0)
+	else if (ld == 0)
 	{
 		printf("Last digit of %d is %d and is zero\n", n, ld);
 	}
--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,26 +1,21 @@
 #include <stdio.h>
 /**
   *mai - Entry point
-  print alphabets in lowercase except q and e
+  *prints the alphabet in lowercase
   *Return: Always 0(success)
   */
 int main(void)
 {
 	int n = 97;
 
-	while (n <= 122);
+	while (n <= 122)
 	{
-		if (n == 101 || n ==113)
+		if (n == 101 || n == 133)
 		{
 			n++;
-			if (n == 97)
-			{
 			continue;
-			}
 		}
-		putchar(n);
-		n++;
+		putchar('\n');
+		return (0);
 	}
-	putchar('\n');
-	return (0);
 }
