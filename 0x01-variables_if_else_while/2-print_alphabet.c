5482b42ec00167088022e267e14160fcd9cd40a0
shanya <shaznyagol@gmail.com>
2023-04-24 00:46:21 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  *Print alphabets in lowercase
+  *Return: Always 0(success0
+  */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
