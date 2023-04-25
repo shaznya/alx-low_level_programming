1833f11acc728432ce74c962537c9eb5ef866b92
shanya <shaznyagol@gmail.com>
2023-04-25 09:10:23 -0700

    main

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,6 +9,7 @@ void print_to_98(int n)
 {
 	int i, j;
 
+
 	if (n <= 98)
 	{
 		for (i = n; i <= 98; i++)
@@ -19,11 +20,13 @@ void print_to_98(int n)
 				printf("%d\n", i);
 		}
 	} else if (n >= 98)
-
+	{
 		for (j = n; j >= 98; j--)
-
+		{
 			if (j != 98)
 				printf("%d, ", j);
 			else if (j == 98)
 				printf("%d\n", j);
+		}
+	}
 }
