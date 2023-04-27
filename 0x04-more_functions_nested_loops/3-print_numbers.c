46bb8b1eb4910f6818f53c3f247c424c75be4b97
shanya <shaznyagol@gmail.com>
2023-04-27 04:28:03 -0700

    print

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  *print_numbers - print 0 to 9
+  *Return: void
+  */
+void print_numbers(void)
+{
+	int a;
+
+	a = 48;
+
+	while (a <= 57)
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+}
