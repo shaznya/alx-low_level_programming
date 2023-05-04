c93ae1d963af4991156b66616f4c09a091242470
shanya <shaznyagol@gmail.com>
2023-05-04 04:44:38 -0700

    compare

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  * _strcmp - compares two strings
+  *@s1: compare
+  *@s2: compare
+  *Return: int
+  */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+	int j = 0;
+
+	if (s1[i] || s2[j] == 0)
+	{
+		i++;
+		j++;
+	}
+	return (0);
+}
