253284e5f06e61d6390ba49780419005a81f44f0
shanya <shaznyagol@gmail.com>
2023-05-04 04:49:25 -0700

    compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,13 +7,16 @@
   */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
-	int j = 0;
+	int i;
 
-	if (s1[i] || s2[j] == 0)
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
 		i++;
-		j++;
 	}
 	return (0);
 }
