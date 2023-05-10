3f7e0d238e1cfdb0a0d5b10e42099bac75c6fc89
shanya <shaznyagol@gmail.com>
2023-05-10 04:25:24 -0700

    reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  *_print_rev_recursion - string in reverse
+  *@S: pointer to string
+  *Return: 0
+  */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
