883d50371c22e7f54d200ea2f3c368be304bb483
shanya <shaznyagol@gmail.com>
2023-05-12 05:57:22 -0700

    args

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  *main -  program that prints its name
+  *@argc: size
+  *@agrv: string
+  *Return: void
+  */
+int main(int argc, *argv[])
+{
+	int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+
+	return (0);
+}
