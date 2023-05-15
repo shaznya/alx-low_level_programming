da4c1b1886cdb05af5e9e270ac6d022059699549
shanya <shaznyagol@gmail.com>
2023-05-15 09:29:16 -0700

    concatenate

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+  *str_concat - concatenates two strings
+  *@s1: dest
+  *@s2: src
+  *Return: char
+  */
+char *str_concat(char *s1, char *s2)
+{
+	char *combo;
+	int i, n;
+
+	if (s1 == NULL)
+		s1 = NULL;
+	if (s2 == NULL)
+		s2 = NULL;
+
+	i = n = 0;
+	while (s1[i] != '\0')
+		i++;
+	while (s2[n] != '\0')
+		n++;
+	combo = malloc(sizeof(char) * (i + n + 1));
+
+	if (combo == NULL)
+		return (NULL);
+	i = n = 0;
+	while (s1[i] != '\0')
+	{
+		combo[i] = s1[i];
+		i++;
+	}
+
+	while (s2[n] != '\0')
+	{
+		combo[i] = s2[n];
+		i++, n++;
+	}
+	combo[i] = '\0';
+	return (combo);
+}
