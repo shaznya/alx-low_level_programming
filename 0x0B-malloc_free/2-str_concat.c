18b0875efc1abe577e8d807a20bbd7f9168f7115
shanya <shaznyagol@gmail.com>
2023-05-16 00:25:04 -0700

    concatenate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,9 +12,11 @@ char *str_concat(char *s1, char *s2)
 	int i, n;
 
 	if (s1 == NULL)
-		s1 = NULL;
+		s1 = "";
+
 	if (s2 == NULL)
-		s2 = NULL;
+		s2 = "";
+
 
 	i = n = 0;
 	while (s1[i] != '\0')
