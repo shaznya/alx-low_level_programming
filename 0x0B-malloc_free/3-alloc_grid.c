285a83bc4e73400fa9b3d5bddb29a19c1b2f380d
shanya <shaznyagol@gmail.com>
2023-05-16 01:02:22 -0700

    dimension

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,67 +9,33 @@
 int **alloc_grid(int width, int height)
 {
 	int **dimen;
-
 	int x, y;
 
-
-
 	if (width <= 0 || height <= 0)
-
 		return (NULL);
-
-
 
 	dimen = malloc(sizeof(int *) * height);
 
-
-
 	if (dimen == NULL)
-
 		return (NULL);
 
-
-
 	for (x = 0; x < height; x++)
-
 	{
-
 		dimen[x] = malloc(sizeof(int) * width);
 
-
-
 		if (dimen[x] == NULL)
-
 		{
-
 			for (; x >= 0; x--)
 
 				free(dimen[x]);
-
-
-
 			free(dimen);
-
 			return (NULL);
-
 		}
-
 	}
-
-
-
 	for (x = 0; x < height; x++)
-
 	{
-
 		for (y = 0; y < width; y++)
-
 			dimen[x][y] = 0;
-
 	}
-
-
-
 	return (dimen);
-
 }
