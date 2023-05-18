723c4368ffcd4a683a9c6e1873ceee4919c7ad3f
shanya <shaznyagol@gmail.com>
2023-05-18 00:06:46 -0700

    mallocchecked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+  *malloc_checked -  allocates memory
+  *@b: size input
+  *Return: pointer to the allocated mem
+  */
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = (int *) malloc(b * sizeof(int));
+
+	if (ptr != NULL)
+	{
+		return (ptr);
+	}
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (0);
+}
