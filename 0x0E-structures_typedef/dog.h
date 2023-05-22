5662f1141f93a048c8b0497d8480b8a6e674faeb
shanya <shaznyagol@gmail.com>
2023-05-22 08:57:59 -0700

    initialize variable

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,10 @@
 #ifndef DOG_H
 #define DOG_H
 /**
-  *struct dog - name of structure
-  *@name: name of dog
-  *@age: age of dog
-  *@owner: owner of dog
+  *struct dog - structure data type
+  *@name: name of structure data type
+  *@age: age of structure data type
+  *@owner: owner of structure data type
   */
 
 struct dog
@@ -14,4 +14,10 @@ struct dog
 	char *owner;
 };
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
