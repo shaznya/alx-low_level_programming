df066344481fcbdaaee4fb825954876ee7434b70
shanya <shaznyagol@gmail.com>
2023-05-22 04:22:17 -0700

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+  *struct dog - name of structure
+  *@name: name of dog
+  *@age: age of dog
+  *@owner: owner of dog
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
