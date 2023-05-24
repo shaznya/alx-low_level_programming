fd797bc9546b44bf129d4ce62800be7842344625
shanya <shaznyagol@gmail.com>
2023-05-24 03:38:57 -0700

    integer index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,10 +11,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (size <= 0)
-	{
+	if (array == NULL || size <= 0 || cmp == NULL)
 		return (-1);
-	}
+
 	while (i < size)
 	{
 		if ((*cmp)(array[i]) != 0)
