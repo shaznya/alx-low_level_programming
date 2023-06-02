44bc628152f2465bfea54bb3b39a58b7b02a0f9a
shaznya <shaznyagol@gmail.com>
2023-06-02 02:55:20 -0700

    C - Singly linked lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void print_text(void) __attribute__((constructor));
+/**
+  *print_text - prints a text
+  *Return: 0
+  */
+void print_text(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
