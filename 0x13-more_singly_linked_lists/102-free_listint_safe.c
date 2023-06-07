c8b111dad9d32c8e2245600015c2086582eff664
shaznya <shaznyagol@gmail.com>
2023-06-07 00:35:48 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,60 +1,40 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
+#include <stdlib.h>
+/**
+  *free_listint_safe - frees a listint_t list.
+  *@h: pointer to ponter to head of listint_t list
+  *Return: size of the list that was free’d
+  */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t length = 0;
+	int difference;
+	listint_t *temp;
 
-typedef struct listint_s {
-    int n;
-    struct listint_s *next;
-} listint_t;
+	if (h == NULL || *h == NULL)
+		return (0);
 
-size_t free_listint_safe(listint_t **h);
-void print_list(listint_t *head);
+	while (*h != NULL)
+	{
+		difference = *h - (*h)->next;
 
-size_t free_listint_safe(listint_t **h) {
-    size_t count = 0;
-    listint_t *current = *h;
-    listint_t *next = NULL;
+		if (difference > 0)
+		{
+			temp = (*h)->next;
+			free(*h);
+			*h = temp;
+			length++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			length++;
+			break;
+		}
+	}
+	*h = NULL;
 
-    while (current != NULL) {
-        next = current->next;
-        free(current);
-        count++;
-        current = next;
-    }
-
-    *h = NULL;
-    return count;
+	return (length);
 }
-
-void print_list(listint_t *head) {
-    while (head != NULL) {
-        printf("[%p] %d\n", (void *)head, head->n);
-        head = head->next;
-    }
-}
-
-int main() {
-    
-    listint_t *head = malloc(sizeof(listint_t));
-    head->n = 0;
-    head->next = malloc(sizeof(listint_t));
-    head->next->n = 1;
-    head->next->next = malloc(sizeof(listint_t));
-    head->next->next->n = 2;
-    head->next->next->next = NULL;
-
-    
-    printf("Before freeing:\n");
-    print_list(head);
-
-    
-    size_t freed_count = free_listint_safe(&head);
-    printf("Freed %zu nodes\n", freed_count);
-
-    
-    printf("After freeing:\n");
-    print_list(head);
-
-    return 0;
-}
-
