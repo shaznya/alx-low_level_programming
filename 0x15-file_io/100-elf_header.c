f68727aa06a1dcfbb815afdddb93924c76d8443a
shaznya <shaznyagol@gmail.com>
2023-06-12 06:39:57 -0700

    this program displays the information contained in the ELF header at the start of an ELF file

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,113 +1,106 @@
-#include <elf.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <elf.h>
 
-void check_elf(unsigned char *e_ident);
-void print_magic(unsigned char *e_ident);
-void print_class(unsigned char *e_ident);
-void print_data(unsigned char *e_ident);
-void print_version(unsigned char *e_ident);
-void print_osabi(unsigned char *e_ident);
-void print_abi(unsigned char *e_ident);
-void print_type(unsigned int e_type);
-void print_entry(unsigned long int e_entry);
-
-void print_elf_header(const char *filename) {
-    int fd = open(filename, O_RDONLY);
-    if (fd == -1) {
-        fprintf(stderr, "Error: Can't read file %s\n", filename);
-        exit(98);
-    }
-
-    Elf64_Ehdr header;
-    ssize_t bytes_read = read(fd, &header, sizeof(Elf64_Ehdr));
-    if (bytes_read == -1 || bytes_read != sizeof(Elf64_Ehdr)) {
-        close(fd);
-        fprintf(stderr, "Error: Failed to read ELF header\n");
-        exit(98);
-    }
-
-    check_elf(header.e_ident);
-
-    printf("ELF Header:\n");
-    print_magic(header.e_ident);
-    print_class(header.e_ident);
-    print_data(header.e_ident);
-    print_version(header.e_ident);
-    print_osabi(header.e_ident);
-    print_abi(header.e_ident);
-    print_type(header.e_type);
-    print_entry(header.e_entry);
-
-    close(fd);
+/**
+ * display_error - Display an error message and exit
+ * @message: The error message to display
+ */
+void display_error(const char *message)
+{
+	fprintf(stderr, "%s\n", message);
+	exit(98);
 }
 
-void check_elf(unsigned char *e_ident) {
-    if (e_ident[EI_MAG0] != ELFMAG0 || e_ident[EI_MAG1] != ELFMAG1 ||
-        e_ident[EI_MAG2] != ELFMAG2 || e_ident[EI_MAG3] != ELFMAG3) {
-        fprintf(stderr, "Error: Not an ELF file\n");
-        exit(98);
-    }
+/**
+ * display_magic - Display the magic numbers from the ELF header
+ * @elf_header: The ELF header structure
+ *
+ * Description: Displays the magic numbers contained in the ELF header.
+ */
+void display_magic(const Elf64_Ehdr *elf_header)
+{
+	int i;
+
+	printf("  Magic:");
+	for (i = 0; i < EI_NIDENT; i++)
+		printf(" %02x", elf_header->e_ident[i]);
+	printf("\n");
 }
 
-void print_magic(unsigned char *e_ident) {
-    printf(" Magic: ");
-    for (int i = 0; i < EI_NIDENT; ++i) {
-        printf("%02x ", e_ident[i]);
-    }
-    printf("\n");
+/**
+ * display_class - Display the class information from the ELF header
+ * @elf_header: The ELF header structure
+ *
+ * Description: Displays the class information from the ELF header.
+ */
+void display_class(const Elf64_Ehdr *elf_header)
+{
+	printf("  Class:                             %s\n",
+	       (elf_header->e_ident[EI_CLASS] == ELFCLASS32) ? "ELF32" : "ELF64");
 }
 
-void print_class(unsigned char *e_ident) {
-    printf(" Class: ");
-    switch (e_ident[EI_CLASS]) {
-        case ELFCLASSNONE:
-            printf("none\n");
-            break;
-        case ELFCLASS32:
-            printf("ELF32\n");
-            break;
-        case ELFCLASS64:
-            printf("ELF64\n");
-            break;
-        default:
-            printf("<unknown: %x>\n", e_ident[EI_CLASS]);
-            break;
-    }
+/**
+ * display_data - Display the data information from the ELF header
+ * @elf_header: The ELF header structure
+ *
+ * Description: Displays the data information from the ELF header.
+ */
+void display_data(const Elf64_Ehdr *elf_header)
+{
+	printf("  Data:%35s%s\n", "", (elf_header->e_ident[EI_DATA] == ELFDATA2LSB) ?
+			"2's complement, little endian" :
+			 "2's complement, big endian");
 }
+/**
+ * main - Entry point of the program
+ * @argc: The argument count
+ * @argv: The argument vector
+ *
+ * Return: 0 on success, 98 on error
+ *
+ * Description: The main function of the program.
+ */
+int main(int argc, char *argv[])
+{
+	const char *file_name;
+	int fd;
+	Elf64_Ehdr elf_header;
+	ssize_t bytes_read;
 
-void print_data(unsigned char *e_ident) {
-    printf(" Data: ");
-    switch (e_ident[EI_DATA]) {
-        case ELFDATANONE:
-            printf("none\n");
-            break;
-        case ELFDATA2LSB:
-            printf("2's complement, little endian\n");
-            break;
-        case ELFDATA2MSB:
-            printf("2's complement, big endian\n");
-            break;
-        default:
-            printf("<unknown: %x>\n", e_ident[EI_DATA]);
-            break;
-    }
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: elf_header elf_filename\n");
+		exit(98);
+	}
+
+	file_name = argv[1];
+	fd = open(file_name, O_RDONLY);
+
+	if (fd == -1)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", file_name);
+		exit(98);
+	}
+	bytes_read = read(fd, &elf_header, sizeof(elf_header));
+
+	if (bytes_read == -1)
+	{
+		fprintf(stderr, "Error: Can't read from file %s\n", file_name);
+		exit(98);
+	}
+
+	printf("ELF Header:\n");
+
+	display_magic(&elf_header);
+	display_class(&elf_header);
+	display_data(&elf_header);
+
+	close(fd);
+
+	return (0);
 }
-
-void print_version(unsigned char *e_ident) {
-    printf(" Version: %d", e_ident[EI_VERSION]);
-    switch (e_ident[EI_VERSION]) {
-        case EV_CURRENT:
-            printf(" (current)\n");
-            break;
-        default:
-            printf("\n");
-            break;
-    }
-}
-
-
