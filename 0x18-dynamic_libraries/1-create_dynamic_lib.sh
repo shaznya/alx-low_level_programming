e46245f5ed43a54a963a78568b4e690c843433f7
shaznya <shaznyagol@gmail.com>
2024-01-30 04:40:05 -0800

    0x18-dynamic_libraries

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
-if [ ! -f 100-operations.c ]; then
-    echo "Error: 100-operations.c not found."
-    exit 1
-fi
-gcc -fPIC -c 100-operations.c
-if [ ! -f 100-operations.o ]; then
-    echo "Error: Compilation of 100-operations.c failed."
-    exit 1
-fi
-gcc -shared -o 100-operations.so 100-operations.o
-if [ ! -f 100-operations.so ]; then
-    echo "Error: Creation of dynamic library failed."
-    exit 1
-fi
-python3 100-tests.py
-rm 100-operations.o
+gcc -fPIC -c *.c
+gcc -shared -o liball.so *.o
+rm *.o
