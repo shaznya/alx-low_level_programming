412a720797140c9a9e2b3a0260a0d85c66c6583e
shaznya <shaznyagol@gmail.com>
2024-01-30 04:04:40 -0800

    0x18-dynamic_libraries

---
+++ 0x18-dynamic_libraries/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  *_puts -  prints a string
+  *@str: print string
+  *Return: 0
+  */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+		_putchar('\n');
+}
